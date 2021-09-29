1a28e250a19e5fcf99914ea9f7f4f4aecb6b01aa
Daniel Marsh-Patrick <daniel@coacervo.co>
2021-09-29 16:54:12 +1300

    Updated Bar templates to include Cross-Filtering and encoding (#6)

--- src/templates/vegaLite/vlBarSimple.ts
+++ src/templates/vegaLite/vlBarSimple.ts
@@ -5,6 +5,7 @@ import {
     vegaDataModelRef,
     vegaLiteProviderInfo
 } from '..';
+import { getConfig } from '../../core/utils/config';
 
 export const vlBarSimple: TopLevelSpec = {
     $schema: vegaLiteProviderInfo,
@@ -18,19 +19,28 @@ export const vlBarSimple: TopLevelSpec = {
     },
     encoding: {
         y: {
-            field: '__yAxis__',
+            field: '__0__',
             type: 'nominal'
         },
         x: {
-            field: '__xAxis__',
+            field: '__1__',
             type: 'quantitative'
+        },
+        opacity: {
+            condition: {
+                test: {
+                    field: '__selected__',
+                    equal: 'off'
+                },
+                value: 0.3
+            }
         }
     },
     usermeta: {
         information: {
             name: 'Simple Bar Chart',
             description:
-                'A simple, single-view bar chart, with a column on the Y-Axis and a measure on the X-Axis.',
+                'A simple, single-view bar chart, with a column on the Y-Axis and a measure on the X-Axis. Also enabled for tooltips and cross-filtering.',
             author: authorInfo,
             uuid: 'ca5fe45d-6b40-4638-baf8-e6c33fdf1d59',
             generated: '2021-03-26T00:00:00.000Z'
@@ -38,7 +48,7 @@ export const vlBarSimple: TopLevelSpec = {
         provider: 'vegaLite',
         dataset: [
             {
-                key: '__yAxis__',
+                key: '__0__',
                 name: 'Category',
                 description:
                     "Select a column that will be displayed on the chart's Y-Axis",
@@ -46,13 +56,20 @@ export const vlBarSimple: TopLevelSpec = {
                 kind: 'column'
             },
             {
-                key: '__xAxis__',
+                key: '__1__',
                 name: 'Measure',
                 description:
                     "Select a measure that will be displayed on the chart's X-Axis",
                 type: 'numeric',
                 kind: 'measure'
             }
-        ]
+        ],
+        interactivity: {
+            tooltip: true,
+            contextMenu: true,
+            selection: true,
+            dataPointLimit:
+                getConfig().propertyDefaults.vega.selectionMaxDataPoints
+        }
     }
 };
