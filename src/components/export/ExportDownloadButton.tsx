853731e82fbe5fa4ded7d858ebeb6205593c434f
Daniel Marsh-Patrick <daniel@coacervo.co>
2021-11-01 16:34:11 +1300

    Fix linting issues

--- src/components/export/ExportDownloadButton.tsx
+++ src/components/export/ExportDownloadButton.tsx
@@ -20,13 +20,13 @@ const ExportDownloadButton: React.FC = () => {
         resolvedName =
             name || i18nValue('Template_Export_Information_Name_Placeholder'),
         handleDownload = () => {
-            /*
-            hostServices.download.exportVisualsContent(
-                getExportTemplate(),
-                `${resolvedName}.json`,
-                'json',
-                i18nValue('Template_Export_Json_File_Description')
-            );
+            /*  Pending API 4.0.0
+                hostServices.download.exportVisualsContent(
+                    getExportTemplate(),
+                    `${resolvedName}.json`,
+                    'json',
+                    i18nValue('Template_Export_Json_File_Description')
+                );
             */
         };
 
--- src/core/services/HostServices.ts
+++ src/core/services/HostServices.ts
@@ -13,13 +13,17 @@ import ITooltipService = powerbi.extensibility.ITooltipService;
 import ISelectionId = powerbi.visuals.ISelectionId;
 import VisualUpdateOptions = powerbi.extensibility.visual.VisualUpdateOptions;
 import IVisualEventService = powerbi.extensibility.IVisualEventService;
-/*import IDownloadService = powerbi.extensibility.IDownloadService;*/
+/*  Pending API 4.0.0
+    import IDownloadService = powerbi.extensibility.IDownloadService;
+*/
 
 /**
  * Proxy service for Power BI host services, plus any additional logic we wish to encapsulate.
  */
 export class HostServices {
-    /*download: IDownloadService;*/
+    /*  Pending API 4.0.0
+        download: IDownloadService;
+    */
     element: HTMLElement;
     events: IVisualEventService;
     fetchMoreData: (aggregateSegments?: boolean) => boolean;
@@ -34,7 +38,9 @@ export class HostServices {
 
     bindHostServices = (options: VisualConstructorOptions) => {
         const { element, host } = options;
-        /*this.download = options.host.downloadService;*/
+        /*  Pending API 4.0.0
+            this.download = options.host.downloadService;
+        */
         this.element = element;
         this.events = options.host.eventService;
         this.fetchMoreData = host.fetchMoreData;
