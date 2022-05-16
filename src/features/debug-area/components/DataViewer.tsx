1a7bf986d101587f36377668d207a88c151a254d
Daniel Marsh-Patrick <daniel@coacervo.co>
2022-05-16 12:01:42 +1200

    Added fallback to visual dataset (#232)
    This enables the creator to see the visual dataset, if the view does not contain any, or is not valid.
    
    Resolves #232

--- src/features/debug-area/components/DataViewer.tsx
+++ src/features/debug-area/components/DataViewer.tsx
@@ -25,6 +25,7 @@ import {
     DATASET_KEY_NAME,
     DATASET_NAME
 } from '../../../constants';
+import { getDataset } from '../../../core/data/dataset';
 
 /**
  * Represents content to be displayed in a data table.
@@ -35,9 +36,20 @@ interface IDataTableContent {
 }
 
 /**
+ * Name of dataset to substitute into the dropdown if the view doesn't contain
+ * any (which will still allow creators to view the underlying Power BI data).
+ */
+const NO_DATASET_NAME = 'dataset';
+
+/**
+ * The name of the root dataset, to filter out when deriving from the view.
+ */
+const ROOT_DATASET_NAME = 'root';
+
+/**
  * Override regular dropdown width styling to handle longer names.
  */
-const dropdownStyles: Partial<IDropdownStyles> = {
+const DROPDOWN_STYLES: Partial<IDropdownStyles> = {
     ...horizontalDropdownStyles,
     ...{ dropdown: { width: 200 } }
 };
@@ -45,10 +57,20 @@ const dropdownStyles: Partial<IDropdownStyles> = {
 /**
  * Obtains all current datasets from a Vega view.
  */
-const getDatasets = (view: View): IDropdownOption[] =>
-    keys(view?.getState({ data: truthy, signals: falsy, recurse: true }).data)
-        .filter((key) => key !== 'root')
-        .map((key) => ({ key, text: key }));
+const getDatasets = (view: View): IDropdownOption[] => {
+    const datasets = keys(getViewData(view)).filter(
+        (key) => key !== ROOT_DATASET_NAME
+    );
+    return (datasets.length === 0 ? [NO_DATASET_NAME] : datasets).map(
+        (key) => ({ key, text: key })
+    );
+};
+
+/**
+ * Apply necessary logic to the view state to get current datasets.
+ */
+const getViewData = (view: View) =>
+    view?.getState({ data: truthy, signals: falsy, recurse: true }).data;
 
 /**
  * Retrieve a dataset from the view by name.
@@ -57,15 +79,16 @@ const getNamedDataset = (view: View, name = DATASET_NAME) => {
     const datasets = getDatasets(view);
     return (
         datasets?.find((d) => d.key === name) ||
-        datasets?.[0] || { key: null, text: 'none' }
+        datasets?.[0] || { key: NO_DATASET_NAME, text: NO_DATASET_NAME }
     );
 };
+
 /**
  * For a dataset view, we need to dynamically get and assign columns from the
  * selected dataset from the view.
  */
 const getDataTableColumns = (dataset: string, view: View): ColumnDefinition[] =>
-    keys(view?.data(dataset)?.[0])
+    keys(getDataTableData(view, dataset)?.[0])
         ?.filter(
             (c) => [DATASET_KEY_NAME, DATASET_IDENTITY_NAME].indexOf(c) === -1
         )
@@ -79,12 +102,31 @@ const getDataTableColumns = (dataset: string, view: View): ColumnDefinition[] =>
         })) || [];
 
 /**
+ * Get the resolved data for the table, accommodating for a potentially
+ * empty Vega view.
+ */
+const getDataTableData = (view: View, name: string) =>
+    viewHasDataset(view, name) ? view?.data(name) : getDataset().values;
+
+/**
  * Obtain table content for display.
  */
 const getDataTableContent = (view: View, name: string): IDataTableContent => ({
     columns: getDataTableColumns(name, view),
-    data: view?.data(name) || []
+    data: getDataTableData(view, name) || []
 });
+
+/**
+ * If our view isn't valid, then we need to substitute the visual dataset, as
+ * it won't be present. This provides a truthiness test for that scenario.
+ */
+const viewHasDataset = (view: View, name: string) => {
+    try {
+        return view?.data(name) !== undefined;
+    } catch (e) {
+        return false;
+    }
+};
 
 /**
  * Handles display of dataset info in the debug area.
@@ -101,7 +143,7 @@ export const DataViewer: React.FC = () => {
     let dataTableContent: IDataTableContent = { columns: [], data: [] };
     try {
         dataTableContent = getDataTableContent(editorView, datasetName);
-    } catch {
+    } catch (e) {
         setSelectedItem(getNamedDataset(editorView));
     }
     const [key, setKey] = useState<number>(0);
@@ -113,9 +155,12 @@ export const DataViewer: React.FC = () => {
         setSelectedItem(item);
     };
     useEffect(() => {
-        editorView?.addDataListener(datasetName, debounceData);
+        viewHasDataset(editorView, datasetName) &&
+            editorView?.addDataListener(datasetName, debounceData);
         return () => {
-            editorView?.removeDataListener(datasetName, debounceData);
+            viewHasDataset(editorView, datasetName)
+                ? editorView?.removeDataListener(datasetName, debounceData)
+                : null;
         };
     });
     reactLog('Rendering [DataViewer]');
@@ -124,7 +169,7 @@ export const DataViewer: React.FC = () => {
             <StackItem>
                 <Stack horizontal horizontalAlign='end'>
                     <Dropdown
-                        styles={dropdownStyles}
+                        styles={DROPDOWN_STYLES}
                         options={dropdownOptions}
                         selectedKey={selectedItem?.key}
                         label={i18nValue('Pivot_Dataset_Select_Label')}
