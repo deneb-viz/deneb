abeb3796bc23a95f936453b0150753d5d3dd8fb6
Daniel Marsh-Patrick <daniel@coacervo.co>
2023-04-03 09:27:56 +1200

    style: update function comments

--- src/core/vega/vegaLiteUtils.ts
+++ src/core/vega/vegaLiteUtils.ts
@@ -5,7 +5,14 @@ import { getState } from '../../store';
 
 export { getPatchedVegaLiteSpec };
 
-// Apply specific patching operations to a supplied spec
+/**
+ * Apply specific patching operations to a supplied spec. This applies any
+ * specific signals that we don't necesairly want the creator to worry about,
+ * but will ensure that the visual functions as expected. We also patch in the
+ * dataset, because we've found that binding this via react-vega causes some
+ * issues with the data being available for certain calculations. This
+ * essentially ensures that the data is processed in-line with the spec.
+ */
 const getPatchedVegaLiteSpec = (spec: TopLevelSpec): TopLevelSpec => ({
     ...spec,
     ...{
