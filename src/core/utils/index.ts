09b0afe24e0ef09a024bdba68adb98661db0f9d0
Daniel Marsh-Patrick <daniel@coacervo.co>
2023-06-07 13:17:21 +1200

    refactor: remove invalid imports

--- src/core/utils/index.ts
+++ src/core/utils/index.ts
@@ -1,8 +1,5 @@
 export * as config from './config';
 export * as features from './features';
-export * as formatting from './formatting';
 export * as json from './json';
-export * as logger from './logger';
 export * as properties from './properties';
-export * as specification from './specification';
 export * as versioning from './versioning';
