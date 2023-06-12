1ab8fdb9a82de7870737e0b64ab8f5bde46597d9
Daniel Marsh-Patrick <daniel@coacervo.co>
2023-06-12 18:17:17 +1200

    chore: update status messages

--- bin/validate-config-for-commit.js
+++ bin/validate-config-for-commit.js
@@ -6,23 +6,25 @@ const errors = [];
 
 // Developer mode: Should not be set in committed code
 if (config.features.developerMode) {
-    errors.push('features.developerMode flag is true; this should be false.');
+    errors.push(
+        '❌ features.developerMode flag is true; this should be false.'
+    );
 }
 // React logging: Should not be set in committed code
 if (config.features.enableReactLogging) {
     errors.push(
-        'features.enableReactLogging flag is true; this should be false.'
+        '❌ features.enableReactLogging flag is true; this should be false.'
     );
 }
 // Log level: should be 0 (NONE) in committed code
 if (config.logLevel !== 0) {
     errors.push(
-        `features.logLevel is ${config.features.logLevel}; this should be 0 (NONE).`
+        `❌ features.logLevel is ${config.features.logLevel}; this should be 0 (NONE).`
     );
 }
 // External URIs: Not permitted in certified visual, so needs to be disabled in committed code.
 if (config.features.enableExternalUri) {
-    errors.push('features.enableExternalUri is true; this should be false.');
+    errors.push('❌ features.enableExternalUri is true; this should be false.');
 }
 
 if (errors.length > 0) {
@@ -32,5 +34,5 @@ if (errors.length > 0) {
     errors.forEach((e, i) => console.error(` ${i + 1}. ${e}`));
     exit(1);
 }
-console.log('No configuration issues found :)');
+console.log('✅ No configuration issues found :)');
 exit(0);
