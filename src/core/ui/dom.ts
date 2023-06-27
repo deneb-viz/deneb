c426025dff3668d55f1e5f6aff4c504fcd5ca72f
Daniel Marsh-Patrick <daniel@coacervo.co>
2023-06-27 13:05:47 +1200

    refactor: removed redundant method

--- src/core/ui/dom.ts
+++ src/core/ui/dom.ts
@@ -48,25 +48,6 @@ export const getZoomOutLevel = (value: number) => {
 export const getEditorHeadingIconClassName = (expanded: boolean) =>
     `editor-${expanded ? 'collapse' : 'expand'}`;
 
-export const getViewModeViewportStyles = (
-    viewport: IViewport,
-    isEditor: boolean,
-    zoomLevel: number,
-    showViewportMarker: boolean
-): React.CSSProperties => {
-    const resolved = `calc(100% - ${viewportAdjust}px)`,
-        scale = calculateZoomLevelPercent(zoomLevel);
-    return {
-        height: isEditor ? viewport.height : resolved,
-        minHeight: isEditor ? viewport.height : resolved,
-        width: isEditor ? viewport.width : resolved,
-        minWidth: isEditor ? viewport.width : resolved,
-        transform: `scale(${scale})`,
-        transformOrigin: '0% 0% 0px',
-        border: showViewportMarker ? '2px dashed #b3b3b3' : null
-    };
-};
-
 /**
  * Convert a value intended for pt to a px equivalent.
  */
