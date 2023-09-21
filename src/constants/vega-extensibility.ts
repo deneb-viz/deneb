9147ab629f01d2d30c41c89cab85c5d494dada0d
Daniel Marsh-Patrick <daniel@coacervo.co>
2023-09-22 11:10:21 +1200

    fix: add custom schemes to Vega Lite schema (#375)

---
+++ src/constants/vega-extensibility.ts
@@ -0,0 +1,16 @@
+/**
+ * Divergent color scheme from Power BI to Vega view.
+ */
+export const VEGA_SCHEME_POWERBI_DIVERGENT = 'pbiColorDivergent';
+/**
+ * Linear color scheme from Power BI to Vega view.
+ */
+export const VEGA_SCHEME_POWERBI_LINEAR = 'pbiColorLinear';
+/**
+ * Nominal color scheme from Power BI to Vega view.
+ */
+export const VEGA_SCHEME_POWERBI_NOMINAL = 'pbiColorNominal';
+/**
+ * Ordinal color scheme from Power BI to Vega view.
+ */
+export const VEGA_SCHEME_POWERBI_ORDINAL = 'pbiColorOrdinal';
