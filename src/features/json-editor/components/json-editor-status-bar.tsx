e7a1289935c026f933fa19cc33ca786858f1bad6
Daniel Marsh-Patrick <daniel@coacervo.co>
2024-05-13 09:59:19 +1200

    feat: re-add back collapse editor pane button (#348)

--- src/features/json-editor/components/json-editor-status-bar.tsx
+++ src/features/json-editor/components/json-editor-status-bar.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import * as ace from 'ace-builds';
 import Ace = ace.Ace;
-import { Caption1, makeStyles } from '@fluentui/react-components';
+import { Caption1, makeStyles, tokens } from '@fluentui/react-components';
 
 import { logRender } from '../../logging';
 import { StatusBarContainer } from '../../interface';
 import { getI18nValue } from '../../i18n';
+import { ToolbarButtonStandard } from '../../toolbar/components/toolbar-button-standard';
+import { PREVIEW_PANE_TOOLBAR_MIN_SIZE } from '../../../constants';
 
 interface IStatusBarProps {
     position: Ace.Point;
@@ -15,9 +17,28 @@ interface IStatusBarProps {
 }
 
 const useStatusStyles = makeStyles({
-    statusBarContainer: {
+    surround: {
+        height: `${PREVIEW_PANE_TOOLBAR_MIN_SIZE}px}`,
+        width: '100%',
+        position: 'absolute',
+        zIndex: 5
+    },
+    container: {
         display: 'flex',
         flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        columnGap: '5px',
+        height: '100%',
+        width: '100%'
+    },
+    collapse: {
+        paddingLeft: tokens.spacingHorizontalXS
+    },
+    status: {
+        display: 'flex',
+        flexDirection: 'row',
+        flexGrow: 1,
         alignItems: 'center',
         justifyContent: 'flex-end',
         columnGap: '20px',
@@ -40,28 +61,41 @@ export const JsonEditorStatusBar: React.FC<IStatusBarProps> = ({
     logRender('JsonEditorStatusBar');
     return (
         <StatusBarContainer>
-            <div
-                className={classes.statusBarContainer}
-                onMouseEnter={clearTokenTooltip}
-                onMouseMove={clearTokenTooltip}
-            >
-                <div className='status-cursor'>
-                    <Caption1>
-                        {getI18nValue('Text_Editor_Status_Bar_Line')} {row}{' '}
-                        {getI18nValue('Text_Editor_Status_Bar_Column')} {column}{' '}
-                        {getSelectedTextMessage(selectedText)}
-                    </Caption1>
+            <div className={classes.surround}>
+                <div className={classes.container}>
+                    <div className={classes.collapse}>
+                        <ToolbarButtonStandard
+                            command='editorPaneToggle'
+                            role='debug'
+                        />
+                    </div>
+                    <div
+                        className={classes.status}
+                        onMouseEnter={clearTokenTooltip}
+                        onMouseMove={clearTokenTooltip}
+                    >
+                        <div className='status-cursor'>
+                            <Caption1>
+                                {getI18nValue('Text_Editor_Status_Bar_Line')}{' '}
+                                {row}{' '}
+                                {getI18nValue('Text_Editor_Status_Bar_Column')}{' '}
+                                {column} {getSelectedTextMessage(selectedText)}
+                            </Caption1>
+                        </div>
+                        <div className='status-tabbing'>
+                            <Caption1>
+                                {escapeHatch
+                                    ? getI18nValue(
+                                          'Text_Editor_Status_Bar_Tab_Focus'
+                                      )
+                                    : getI18nValue(
+                                          'Text_Editor_Status_Bar_Tab_Edit'
+                                      )}{' '}
+                            </Caption1>
+                        </div>
+                        <div className='status-settings'></div>
+                    </div>
                 </div>
-                <div className='status-tabbing'>
-                    <Caption1>
-                        {escapeHatch
-                            ? getI18nValue('Text_Editor_Status_Bar_Tab_Focus')
-                            : getI18nValue(
-                                  'Text_Editor_Status_Bar_Tab_Edit'
-                              )}{' '}
-                    </Caption1>
-                </div>
-                <div className='status-settings'></div>
             </div>
         </StatusBarContainer>
     );
