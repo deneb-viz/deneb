c4d9ba93c5ff0f78de15afc2a0fd6300642c77b2
Daniel Marsh-Patrick <daniel@coacervo.co>
2025-07-07 09:18:42 +1200

    chore: update dependencies for current certification requirements

---
+++ eslint.config.mjs
@@ -0,0 +1,15 @@
+import powerbiVisualsConfigs from 'eslint-plugin-powerbi-visuals';
+
+export default [
+    powerbiVisualsConfigs.configs.recommended,
+    {
+        ignores: [
+            'node_modules/**',
+            'dist/**',
+            '.vscode/**',
+            '.tmp/**',
+            'packages/**',
+            'bin/**'
+        ]
+    }
+];
