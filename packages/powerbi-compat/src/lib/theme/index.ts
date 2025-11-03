dc0c41c399a0ccd241b8b5ffc8e7d467a440647c
Daniel Marsh-Patrick <daniel@coacervo.co>
2025-11-04 10:24:21 +1300

    styles: update for prettier configuration

--- packages/powerbi-compat/src/lib/theme/index.ts
+++ packages/powerbi-compat/src/lib/theme/index.ts
@@ -1,5 +1,5 @@
-export { POWERBI_THEME_DEFAULT } from "./shim";
+export { POWERBI_THEME_DEFAULT } from './shim';
 export type {
-  PowerBiColorPalette,
-  PowerBIColorPaletteExtension,
-} from "./types";
+    PowerBiColorPalette,
+    PowerBIColorPaletteExtension
+} from './types';
