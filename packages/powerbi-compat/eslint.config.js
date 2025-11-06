2b022c48c11aa42db2193020a18379f5c2eb5502
Daniel Marsh-Patrick <daniel@coacervo.co>
2025-11-06 17:36:51 +1300

    chore: update eslint configuration

--- packages/powerbi-compat/eslint.config.js
+++ packages/powerbi-compat/eslint.config.js
@@ -1,4 +1,4 @@
-import { config } from '@deneb-viz/eslint-config/base';
+import { config } from '@deneb-viz/eslint-config/base.js';
 
 /** @type {import("eslint").Linter.Config} */
 export default [...config];
