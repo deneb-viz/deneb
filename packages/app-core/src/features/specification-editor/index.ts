896e3110362c5ad01222555f215fc1610fbe2be9
Daniel Marsh-Patrick <daniel@coacervo.co>
2025-12-03 21:35:21 +1100

    refactor: move to app-core

--- packages/app-core/src/features/specification-editor/index.ts
+++ packages/app-core/src/features/specification-editor/index.ts
@@ -1,5 +1,6 @@
 export { ProviderDetail } from './components/provider-detail';
 export { SpecificationEditorProvider } from './components/specification-editor-provider';
+export { SpecificationEditorStatusBar } from './components/specification-editor-status-bar';
 export { TrackingSyncStatus } from './components/tracking-sync-status';
 export { useSpecificationEditor } from './hooks/use-specification-editor';
 export type * from './types';
