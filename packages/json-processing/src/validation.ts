28205d9dede022ed44008442b4856c63025406c7
Daniel Marsh-Patrick <daniel@coacervo.co>
2026-01-07 15:45:12 +1300

    fix: schema import syntax for non-webpack bundlers

--- packages/json-processing/src/validation.ts
+++ packages/json-processing/src/validation.ts
@@ -10,10 +10,10 @@ import {
 /**
  * All schemas required for validation functions.
  */
-import * as vegaSchema from 'vega/vega-schema.json';
-import * as vegaLiteSchema from 'vega-lite/vega-lite-schema.json';
-import * as draft06Schema from 'ajv/lib/refs/json-schema-draft-06.json';
-import * as denebUserMetaSchema from '@deneb-viz/template-usermeta/schema.deneb-template-usermeta.json';
+import vegaSchema from 'vega/vega-schema.json';
+import vegaLiteSchema from 'vega-lite/vega-lite-schema.json';
+import draft06Schema from 'ajv/lib/refs/json-schema-draft-06.json';
+import denebUserMetaSchema from '@deneb-viz/template-usermeta/schema.deneb-template-usermeta.json';
 
 /**
  * Create a common validator, with the necessary schema support for Vega/
