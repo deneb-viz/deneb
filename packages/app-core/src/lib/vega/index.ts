277613b21469aa9ec6e39cd131ec3c85060c8266
Daniel Marsh-Patrick <daniel@coacervo.co>
2026-03-17 12:41:24 +1300

    fix: remove createSchemaValidator from lib/vega barrel (#591)

--- packages/app-core/src/lib/vega/index.ts
+++ packages/app-core/src/lib/vega/index.ts
@@ -1,3 +1,2 @@
 export { INCREMENTAL_UPDATE_CONFIGURATION } from './incremental-update-configuration';
-export { createSchemaValidator } from './compilation';
 export { getVegaProviderI18n } from './i18n';
