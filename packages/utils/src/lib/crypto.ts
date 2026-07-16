e7a7f9e95ddc58940e2f5c6f462fcc9ce7f337c7
Daniel Marsh-Patrick <daniel@coacervo.co>
2026-07-17 11:59:39 +1200

    chore: simplify UUID variant-bit expression

--- packages/utils/src/lib/crypto.ts
+++ packages/utils/src/lib/crypto.ts
@@ -16,7 +16,7 @@ export function getNewUuid() {
     }
     return 'xxxxxxxx-xxxx-4xxx-Nxxx-xxxxxxxxxxxx'
         .replace(/x/g, () => ((Math.random() * 16) | 0).toString(16))
-        .replace(/N/g, () => ((Math.random() * 4) | (0 + 8)).toString(16));
+        .replace(/N/g, () => ((Math.random() * 4) | 8).toString(16));
 }
 
 /**
